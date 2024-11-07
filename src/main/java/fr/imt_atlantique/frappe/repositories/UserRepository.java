31b274ee3452e039fba4a43c5710c2330b48396b
Tchadel Icard <tchadelicard@protonmail.com>
2024-11-07 14:38:05 +0100

    controller/repo: readd imports for user class

--- src/main/java/fr/imt_atlantique/frappe/repositories/UserRepository.java
+++ src/main/java/fr/imt_atlantique/frappe/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package fr.imt_atlantique.frappe.repositories;
 
+import fr.imt_atlantique.frappe.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
