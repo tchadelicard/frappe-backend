5c3d7290f51c40d261f1cb705aff7607a6a18448
Tchadel Icard <tchadelicard@protonmail.com>
2024-11-07 22:28:17 +0100

    schema: update database schema

--- schema.sql
+++ schema.sql
@@ -13,17 +13,17 @@ DROP TABLE IF EXISTS supervisors;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE campuses (
-    id SERIAL PRIMARY KEY,
+    campus_id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE specialties (
-    id SERIAL PRIMARY KEY,
+    specialty_id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    user_id BIGSERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
@@ -31,19 +31,19 @@ CREATE TABLE users (
     email VARCHAR(255) NOT NULL,
     phone_number VARCHAR(255) NOT NULL,
     campus_id INTEGER NOT NULL,
-    FOREIGN KEY (campus_id) REFERENCES campuses(id)
+    FOREIGN KEY (campus_id) REFERENCES campuses(campus_id)
 );
 
 CREATE TABLE supervisors (
-    id INTEGER PRIMARY KEY,
+    supervisor_id BIGINT PRIMARY KEY,
     meeting_url VARCHAR(255),
     caldav_username VARCHAR(255),
     caldav_password VARCHAR(255),
-    FOREIGN KEY (id) REFERENCES users(id)
+    FOREIGN KEY (supervisor_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE credit_transfers (
-    id SERIAL PRIMARY KEY,
+    credit_transfer_id BIGSERIAL PRIMARY KEY,
     university VARCHAR(255) NOT NULL,
     country VARCHAR(255) NOT NULL,
     start_date DATE NOT NULL,
@@ -51,70 +51,70 @@ CREATE TABLE credit_transfers (
 );
 
 CREATE TABLE students (
-    id INTEGER PRIMARY KEY,
+    student_id BIGINT PRIMARY KEY,
     gender VARCHAR(255) NOT NULL,
     nationality VARCHAR(255) NOT NULL,
     credit_transfer_id INTEGER,
-    FOREIGN KEY (id) REFERENCES users(id),
-    FOREIGN KEY (credit_transfer_id) REFERENCES credit_transfers(id)
+    FOREIGN KEY (student_id) REFERENCES users(user_id),
+    FOREIGN KEY (credit_transfer_id) REFERENCES credit_transfers(credit_transfer_id)
 );
 
 CREATE TABLE curriculums (
-    id SERIAL PRIMARY KEY,
+    curriculum_id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE students_curriculums (
-    student_id INTEGER NOT NULL,
-    curriculum_id INTEGER NOT NULL,
+    student_id BIGINT NOT NULL,
+    curriculum_id BIGINT NOT NULL,
     year INTEGER NOT NULL,
-    FOREIGN KEY (student_id) REFERENCES students(id),
-    FOREIGN KEY (curriculum_id) REFERENCES curriculums(id),
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (curriculum_id) REFERENCES curriculums(curriculum_id),
     PRIMARY KEY (student_id, curriculum_id)
 );
 
 CREATE TABLE campuses_specialties (
-    campus_id INTEGER NOT NULL,
-    specialty_id INTEGER NOT NULL,
-    FOREIGN KEY (campus_id) REFERENCES campuses(id),
-    FOREIGN KEY (specialty_id) REFERENCES specialties(id),
+    campus_id BIGINT NOT NULL,
+    specialty_id BIGINT NOT NULL,
+    FOREIGN KEY (campus_id) REFERENCES campuses(campus_id),
+    FOREIGN KEY (specialty_id) REFERENCES specialties(specialty_id),
     PRIMARY KEY (campus_id, specialty_id)
 );
 
 CREATE TABLE students_specialties_per_year (
-    student_id INTEGER NOT NULL,
-    specialty_id INTEGER NOT NULL,
+    student_id BIGINT NOT NULL,
+    specialty_id BIGINT NOT NULL,
     year INTEGER NOT NULL,
-    FOREIGN KEY (student_id) REFERENCES students(id),
-    FOREIGN KEY (specialty_id) REFERENCES specialties(id),
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (specialty_id) REFERENCES specialties(specialty_id),
     PRIMARY KEY (student_id, specialty_id)
 );
 
 CREATE TABLE meeting_requests (
-    id SERIAL PRIMARY KEY,
+    meeting_request_id BIGSERIAL PRIMARY KEY,
     date DATE NOT NULL,
     duration INTEGER NOT NULL,
     theme VARCHAR(255) NOT NULL,
     location VARCHAR(255) NOT NULL,
     request_description TEXT NOT NULL,
     status VARCHAR(255) NOT NULL,
-    action_id INTEGER,
-    student_id INTEGER NOT NULL,
-    supervisor_id INTEGER NOT NULL,
-    FOREIGN KEY (student_id) REFERENCES students(id),
-    FOREIGN KEY (supervisor_id) REFERENCES supervisors(id)
+    action_id BIGINT,
+    student_id BIGINT NOT NULL,
+    supervisor_id BIGINT NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (supervisor_id) REFERENCES supervisors(supervisor_id)
 );
 
 CREATE TABLE actions (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     notes TEXT NOT NULL,
     action_plan TEXT NOT NULL
 );
 
 CREATE TABLE internship_requests (
-    id INTEGER PRIMARY KEY,
+    internship_request_id BIGINT PRIMARY KEY,
     internship_duration INTEGER NOT NULL,
     wanted_city VARCHAR(255) NOT NULL,
     wanted_country VARCHAR(255) NOT NULL,
-    FOREIGN KEY (id) REFERENCES meeting_requests(id)
+    FOREIGN KEY (internship_request_id) REFERENCES meeting_requests(meeting_request_id)
 );
