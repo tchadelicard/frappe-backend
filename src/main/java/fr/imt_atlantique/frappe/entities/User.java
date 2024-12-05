5c8358e1ab3728953c4ffdcee31a6814b914b66d
Tchadel Icard <tchadelicard@protonmail.com>
2024-12-05 09:57:33 +0100

    feat/entities: remove not null constraints

--- src/main/java/fr/imt_atlantique/frappe/entities/User.java
+++ src/main/java/fr/imt_atlantique/frappe/entities/User.java
@@ -42,7 +42,7 @@ public class User implements UserDetails {
     @Column(name = "email", nullable = false)
     private String email;
 
-    @Column(name = "phone_number", nullable = false)
+    @Column(name = "phone_number")
     private String phoneNumber;
 
     @Column(name = "enabled", nullable = false)
@@ -55,7 +55,7 @@ public class User implements UserDetails {
     private Instant validationCodeExpiry;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "campus_id", nullable = false)
+    @JoinColumn(name = "campus_id")
     private Campus campus;
 
     @Override
