84118e7bfa37272d8b25d6ebdc91e0f49efc5919
Tchadel Icard <tchadelicard@protonmail.com>
2024-12-12 14:32:01 +0100

    feat/profile: allow all HTTP methods

--- src/main/java/fr/imt_atlantique/frappe/configs/SecurityConfig.java
+++ src/main/java/fr/imt_atlantique/frappe/configs/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
