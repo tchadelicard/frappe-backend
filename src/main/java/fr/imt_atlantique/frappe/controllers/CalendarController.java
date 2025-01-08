49028a89de83fdf562e9978e37b9b5d3c2469997
riadh <riadh.ben-mustapha@imt-atlantique.net>
2025-01-08 15:53:54 +0100

    correction des api av

--- src/main/java/fr/imt_atlantique/frappe/controllers/CalendarController.java
+++ src/main/java/fr/imt_atlantique/frappe/controllers/CalendarController.java
@@ -3,6 +3,7 @@ package fr.imt_atlantique.frappe.controllers;
 import fr.imt_atlantique.frappe.services.CalendarService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import fr.imt_atlantique.frappe.dtos.*;
 
@@ -23,11 +24,11 @@ public class CalendarController {
 
 
     @GetMapping("/availabilities")
-    public List<Map<String, Object>> getAvailabilities(@RequestBody AvailabilityRequest request ) {
-        return calendarService.getAvailabilities(request.getDuration());
+    public List<Map<String, Object>> getAvailabilities(@RequestParam String supervisorid  ,@RequestParam  String duration )  {
+        return calendarService.getAvailabilities(duration);
     }
     @GetMapping("/dailyavailabilities")
-    public List<Map<String, Object>> getDailyAvailabilities(@RequestBody DailyAvailabilityRequest request ) {
-        return calendarService.getDailyAvailabilities(request.getDate() ,request.getDuration());
+    public List<Map<String, Object>> getDailyAvailabilities(@RequestParam String supervisorid  ,@RequestParam  String duration, @RequestParam String date)  {
+        return calendarService.getDailyAvailabilities(date , duration);
     }
 }
