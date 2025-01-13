a7198f6415ffcf6466808b1797ac5298f4fb4bba
Tchadel Icard <tchadelicard@protonmail.com>
2025-01-13 21:02:28 +0100

    feat/availabilities: use a date range for the /days endpoint

--- src/main/java/fr/imt_atlantique/frappe/dtos/GetAvailableDaysRequest.java
+++ src/main/java/fr/imt_atlantique/frappe/dtos/GetAvailableDaysRequest.java
@@ -3,10 +3,14 @@ package fr.imt_atlantique.frappe.dtos;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 @Builder
 public class GetAvailableDaysRequest {
     String username;
     String password;
     String duration;
+    LocalDate startDate;
+    LocalDate endDate;
 }
