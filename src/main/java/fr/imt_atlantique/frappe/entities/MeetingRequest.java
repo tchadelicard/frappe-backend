90733b94dc97eb9d0c55467d1f1516d05a9b6e99
Tchadel Icard <tchadelicard@protonmail.com>
2025-01-13 23:24:14 +0100

    db: update meeting requests to have a start and end date

--- src/main/java/fr/imt_atlantique/frappe/entities/MeetingRequest.java
+++ src/main/java/fr/imt_atlantique/frappe/entities/MeetingRequest.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -18,11 +19,11 @@ public class MeetingRequest {
     @Column(name = "meeting_request_id", nullable = false)
     private Long id;
 
-    @Column(name = "date", nullable = false)
-    private LocalDate date;
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
 
-    @Column(name = "duration", nullable = false)
-    private Integer duration;
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
 
     @Column(name = "theme", nullable = false)
     private String theme;
