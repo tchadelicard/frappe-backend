746ad5f2ee62aab505bce5dd44d464a31b434adc
Tchadel Icard <tchadelicard@protonmail.com>
2025-02-27 16:27:12 +0100

    refactor: CreditTransferController and CreditTransferService

--- src/main/java/fr/imt_atlantique/frappe/controllers/CreditTransferController.java
+++ src/main/java/fr/imt_atlantique/frappe/controllers/CreditTransferController.java
@@ -1,8 +1,8 @@
 package fr.imt_atlantique.frappe.controllers;
 
 import fr.imt_atlantique.frappe.dtos.CreditTransferDTO;
-import fr.imt_atlantique.frappe.entities.CreditTransfer;
 import fr.imt_atlantique.frappe.services.CreditTransferService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -21,18 +21,20 @@ public class CreditTransferController {
         this.creditTransferService = creditTransferService;
     }
 
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity<List<CreditTransferDTO>> getCreditTransfers(
-            @RequestParam(required = false) String university,
-            @RequestParam(required = false) String country,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
-    ) {
-        return creditTransferService.findByFilters(university, country, startDate, endDate);
+            @Valid @RequestParam(required = false) String university,
+            @Valid @RequestParam(required = false) String country,
+            @Valid @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @Valid @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+        List<CreditTransferDTO> creditTransfers = creditTransferService.findByFilters(university, country, startDate,
+                endDate);
+        return ResponseEntity.ok(creditTransfers);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CreditTransferDTO> getCreditTransfer(@PathVariable @Min(1) Long id) {
-        return creditTransferService.findById(id);
+    public ResponseEntity<CreditTransferDTO> getCreditTransfer(@Valid @PathVariable @Min(1) Long id) {
+        CreditTransferDTO creditTransfer = creditTransferService.findById(id);
+        return ResponseEntity.ok(creditTransfer);
     }
 }
--- src/main/java/fr/imt_atlantique/frappe/services/CreditTransferService.java
+++ src/main/java/fr/imt_atlantique/frappe/services/CreditTransferService.java
@@ -2,17 +2,15 @@ package fr.imt_atlantique.frappe.services;
 
 import fr.imt_atlantique.frappe.dtos.CreditTransferDTO;
 import fr.imt_atlantique.frappe.entities.CreditTransfer;
+import fr.imt_atlantique.frappe.exceptions.CreditTransferNotFoundException;
 import fr.imt_atlantique.frappe.repositories.CreditTransferRepository;
 import fr.imt_atlantique.frappe.specifications.CreditTransferSpecification;
-import jakarta.validation.constraints.Min;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CreditTransferService {
@@ -24,32 +22,22 @@ public class CreditTransferService {
         this.modelMapper = modelMapper;
     }
 
-    public ResponseEntity<List<CreditTransferDTO>> findByFilters(String university, String country, LocalDate startDate, LocalDate endDate) {
+    public List<CreditTransferDTO> findByFilters(String university, String country, LocalDate startDate,
+            LocalDate endDate) {
         Specification<CreditTransfer> spec = Specification
                 .where(CreditTransferSpecification.hasUniversity(university))
                 .and(CreditTransferSpecification.hasCountry(country))
                 .and(CreditTransferSpecification.startDateAfter(startDate))
                 .and(CreditTransferSpecification.endDateBefore(endDate));
 
-        List<CreditTransfer> creditTransfers = creditTransferRepository.findAll(spec);
-
-        if (creditTransfers.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        List<CreditTransferDTO> creditTransfersDTO = creditTransfers.stream()
+        return creditTransferRepository.findAll(spec).stream()
                 .map(creditTransfer -> modelMapper.map(creditTransfer, CreditTransferDTO.class))
                 .toList();
-
-        return ResponseEntity.ok(creditTransfersDTO);
     }
 
-    public ResponseEntity<CreditTransferDTO> findById(@Min(1) Long id) {
-        Optional<CreditTransfer> creditTransfer = creditTransferRepository.findById(id);
-        if (creditTransfer.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-        CreditTransferDTO creditTransferDTO = modelMapper.map(creditTransfer.get(), CreditTransferDTO.class);
-        return ResponseEntity.ok(creditTransferDTO);
+    public CreditTransferDTO findById(Long id) {
+        return creditTransferRepository.findById(id)
+                .map(creditTransfer -> modelMapper.map(creditTransfer, CreditTransferDTO.class))
+                .orElseThrow(() -> new CreditTransferNotFoundException(Long.toString(id)));
     }
 }
