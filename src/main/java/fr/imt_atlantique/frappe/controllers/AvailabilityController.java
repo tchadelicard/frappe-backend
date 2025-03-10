66947540987f95c1a8c3be7e5f708a4942fe2527
Tchadel Icard <tchadelicard@protonmail.com>
2025-03-10 17:58:32 +0100

    fix: use explicit type in the ResponseEntity of getAvailableSlotsForSupervisor

--- src/main/java/fr/imt_atlantique/frappe/controllers/AvailabilityController.java
+++ src/main/java/fr/imt_atlantique/frappe/controllers/AvailabilityController.java
@@ -1,5 +1,6 @@
 package fr.imt_atlantique.frappe.controllers;
 
+import fr.imt_atlantique.frappe.dtos.AvailabilitySlotDTO;
 import fr.imt_atlantique.frappe.services.AvailabilityService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,12 +24,13 @@ public class AvailabilityController {
             @RequestParam String duration,
             @RequestParam LocalDate startDate,
             @RequestParam LocalDate endDate) {
-        List<LocalDate> availableDays = availabilityService.getAvailableDaysForSupervisor(id, duration, startDate, endDate);
+        List<LocalDate> availableDays = availabilityService.getAvailableDaysForSupervisor(id, duration, startDate,
+                endDate);
         return ResponseEntity.ok(availableDays);
     }
 
     @GetMapping("/{id}/slots")
-    public ResponseEntity<?> getAvailableSlotsForSupervisor(
+    public ResponseEntity<List<AvailabilitySlotDTO>> getAvailableSlotsForSupervisor(
             @PathVariable Long id,
             @RequestParam LocalDate date,
             @RequestParam String duration) {
