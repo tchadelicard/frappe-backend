8aa169e4669dd9a71aac26088953dc75ace16168
Tchadel Icard <tchadelicard@protonmail.com>
2025-03-17 19:44:34 +0100

    chores: remove useless imports

--- src/main/java/fr/imt_atlantique/frappe/filters/JwtAuthFilter.java
+++ src/main/java/fr/imt_atlantique/frappe/filters/JwtAuthFilter.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
