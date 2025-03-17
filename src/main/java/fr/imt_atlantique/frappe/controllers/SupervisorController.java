54b7fd5e82110da3b97e36b4b4daff81e8098824
Tchadel Icard <tchadelicard@protonmail.com>
2025-03-17 19:44:34 +0100

    chores: remove useless imports

--- src/main/java/fr/imt_atlantique/frappe/controllers/SupervisorController.java
+++ src/main/java/fr/imt_atlantique/frappe/controllers/SupervisorController.java
@@ -4,7 +4,6 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
